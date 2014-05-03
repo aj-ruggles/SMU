b22ec5a95263362bd3cb4369a6206b2a67edec80
ar0020 <austinruggles@gmail.com>
2014-05-03 01:01:56 -0500

    Initial Application SMU - scheduling management utility

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SMU::Application.config.secret_key_base = '8a50e45c5fda7c678944362589fb47be7000632563225e2df3cc2d3fd12e3bb3cb3609ceb4be2635504dcc8469b3066d20fb7a547baa87fd76200b6b2c19822f'
